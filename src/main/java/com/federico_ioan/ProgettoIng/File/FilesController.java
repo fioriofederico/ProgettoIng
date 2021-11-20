181d25513895897b40a5a808c557d829e5c49327
ioan <i.corrias@libero.it>
2021-11-20 13:08:45 +0100

    Checkpoint

--- src/main/java/com/federico_ioan/ProgettoIng/File/FilesController.java
+++ src/main/java/com/federico_ioan/ProgettoIng/File/FilesController.java
@@ -1,23 +1,21 @@
 package com.federico_ioan.ProgettoIng.File;
 
 import java.security.MessageDigest;
+import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
+import com.google.api.client.util.NullValue;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 
@@ -28,7 +26,7 @@ import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBui
 public class FilesController {
 
   private final FileInfoRepository fileInfoRepository;
-	
+
   @Autowired
   FilesStorageService storageService;
 
@@ -39,37 +37,43 @@ public class FilesController {
   @PostMapping("/files")
   public ResponseEntity<ResponseMessage> uploadFile(@RequestParam("file") MultipartFile file) {
 
+    // Initilize the response message
     String message = "";
+    LocalDateTime localDateTime  = LocalDateTime.now(ZoneId.of("GMT+01:00"));
+    String fileName = file.getOriginalFilename();
+    // Create a url to hash
+    String urlToHash = fileName + Timestamp.valueOf(localDateTime);
+
     try {
-      LocalDateTime localDateTime  = LocalDateTime.now(ZoneId.of("GMT+01:00"));
-      String fileName = file.getOriginalFilename();
-      String stringToHash = fileName + localDateTime.toString();
-
-      // Hashing the filename
-      byte[] bytesOfMessage = stringToHash.getBytes("UTF-8");
+      // Hash the url
+      byte[] bytesOfUrl = urlToHash.getBytes("UTF-8");
       MessageDigest md = MessageDigest.getInstance("MD5");
-      byte[] theMD5digest = md.digest(bytesOfMessage);
+      byte[] digestedUrl = md.digest(bytesOfUrl);
 
-      String newFileName = theMD5digest.toString() + "." + file.getOriginalFilename().split("\\.")[1];
+      // Build the final url
+      String fileUrl = digestedUrl.toString() + "." + file.getOriginalFilename().split("\\.")[1];
 
-      storageService.save(file, newFileName);
+      // Save file in the server
+      storageService.save(file, fileUrl);
 
-      // Save into the db
-      FileInfo fileInfo = new FileInfo(fileName, newFileName, localDateTime);
+      // Save file info in the db
+      FileInfo fileInfo = new FileInfo(fileName, fileUrl, localDateTime);
       fileInfoRepository.save(fileInfo);
 
-
-      message = "Uploaded the file successfully: " + file.getOriginalFilename();
+      // Return response message
+      message = "Uploaded the file successfully: " + fileName;
       return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage(message));
     } catch (Exception e) {
-      message = "Could not upload the file: " + file.getOriginalFilename() + "!";
+      message = "Could not upload the file: " + fileName + "!";
       return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage(message));
     }
   }
 
+  // TO DO: add delete file API function
+
   @GetMapping("/files")
   public ResponseEntity<List<FileInfo>> getListFiles() {
-    List<FileInfo> fileInfos = storageService.loadAll().map(path -> {
+    List<FileInfo> fileInfos = storageService.loadAll().map(path -> {   // TO DO: get FileInfo from db
       String filename = path.getFileName().toString();
       String url = MvcUriComponentsBuilder .fromMethodName(FilesController.class, "getFile",
               path.getFileName().toString()).build().toString();
